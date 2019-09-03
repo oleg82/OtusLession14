356e8aaffd03ffc6eced32645a5bb53f419b99ed
Олег Иванов <oivanov@mkb.ru>
2019-09-03 22:28:29 +0300

    Update version GithubManager.git

--- OtusLession14/Dependencies/Package.swift
+++ OtusLession14/Dependencies/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Dependencies"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/oleg82/GithubManager.git", from: "1.0.0"),
+        .package(url: "https://github.com/oleg82/GithubManager.git", from: "1.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
